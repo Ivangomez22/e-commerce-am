d5f4e8cd8ac50d85e1433e2aeb40a6671cf6f4be
Jesus Castro <jtcastro98@gmail.com>
2021-11-23 21:32:00 -0500

    Actividad: Card Buttons.

--- src/App.js
+++ src/App.js
@@ -24,8 +24,23 @@ const App = () => {
     }
 
     const handleAddToCart = async (productId, quantity) => {
-        const item = await commerce.cart.add(productId, quantity);
-        setCart(item.cart);
+        const { cart } = await commerce.cart.add(productId, quantity);
+        setCart(cart);
+    }
+
+    const handleUpdateCartQty = async (productoId, quantity) => {
+        const { cart } = await commerce.cart.update(productoId, { quantity })
+        setCart(cart);
+    }
+
+    const handleRemoveFromCart = async (productId) => {
+        const { cart } = await commerce.cart.remove(productId);
+        setCart(cart);
+    }
+
+    const handleEmptyCart = async () => {
+        const { cart } = await commerce.cart.empty();
+        setCart(cart);
     }
 
     useEffect(() => {
@@ -46,7 +61,12 @@ const App = () => {
                         </Route>
                         
                         <Route exact path="/cart">
-                            <Cart cart={cart} />
+                            <Cart 
+                                cart={cart} 
+                                handleUpdateCartQty  = { handleUpdateCartQty } 
+                                handleRemoveFromCart = { handleRemoveFromCart }
+                                handleEmptyCart      = { handleEmptyCart }
+                            />
                         </Route>
                     </Switch>                
             </div>
